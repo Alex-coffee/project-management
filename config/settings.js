d1e11b2642531c841dd0d5f084029360c629b5a3
19616158 <19616158@qq.com>
2017-09-21 21:26:59 +0800

    change db path

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-    dbURL: "mongodb://10.37.129.2:27017/projectManagement",
-    // dbURL: "mongodb://localhost:27017/projectManagement",
+    // dbURL: "mongodb://10.37.129.2:27017/projectManagement",
+    dbURL: "mongodb://localhost:27017/projectManagement",
     dataPath : "src/",
     outputPath : "src/assets/or/output/",
     orInputPath : "src/assets/or/temp/",
