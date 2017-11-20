a44c23ec24dd382488093eabd9051c18f66f5a79
19616158 <19616158@qq.com>
2017-11-20 15:38:05 +0800

    changes for the dbURL

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,12 @@
 module.exports = {
     // dbURL: "mongodb://10.37.129.2:27017/projectManagement",
-    //dbURL: "mongodb://localhost:27017/projectManagement",
-    dbURL: "mongodb://bigboundAdmin:P0o9i8u7@115.159.83.14:27017/projectManagement",
+    // dbURL: "mongodb://localhost:27017/projectManagement",
+    dbURL: "mongodb://bigboundAdmin:P0o9i8u7@115.159.83.14:27017/projectManagement?authSource=admin",
     dataPath : "src/",
     outputPath : "src/assets/or/output/",
     orInputPath : "src/assets/or/temp/",
-    systemPath: "Z:/Users/Alex.Chen/git/project-management-windows/src/assets/or/",
-    //systemPath: "src/assets/or/",
+    // systemPath: "Z:/Users/Alex.Chen/git/project-management-windows/src/assets/or/",
+    systemPath: "src/assets/or/",
     uploadPath: "src/assets/or/upload/",
     command: "ProductionPlan",
     secret: "bigbound"
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 // export const HTTP_BASE = "http://10.211.55.4:8001";
-export const HTTP_BASE = "http://10.211.55.6:8001";
-//export const HTTP_BASE = "http://localhost:8001";
+// export const HTTP_BASE = "http://10.211.55.6:8001";
+export const HTTP_BASE = "http://localhost:8001";
